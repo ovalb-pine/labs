810409aab084cda77469df4713dce8204ff89445
ovalb-pine <maalefremova@edu.hse.ru>
2024-11-22 19:22:23 +0300

    2

--- 2/graph_all.py
+++ 2/graph_all.py
@@ -7,8 +7,12 @@ for file_number in range(1, 4):
     # Set up path
     csv_file_path = f"2/csv/{file_number}.csv"
     df = pd.read_csv(csv_file_path, sep="\t", header=None)
+    plt.style.use("seaborn-v0_8")
     plt.figure(figsize=(6, 4)) 
-    for i in range (1, 15):
-        plt.plot(df[1: i], df[0])
+    x = df[0]  # Use the 0th column as x
+    for col in df.columns[1:]:
+        y = df[col] + 273.15  # Use the current column as y
+        plt.plot(x, y, label=f"{col}")
+    plt.legend(facecolor='white', framealpha=0.6, frameon=1)
     plt.show()
     plt.close()   
